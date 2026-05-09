76062364da63332d32c3218bd4f5a66f00ab9e33
JadielSilva11 <jdielcunha@gmail.com>
2026-05-09 17:41:04 -0300

    Adicionando verificações no código js do exercício

--- aula14/script.js
+++ aula14/script.js
@@ -3,9 +3,12 @@ function contador(){
     let fim = Number(document.getElementById('fim').value);
     let passos = Number(document.getElementById('passos').value);
     let p = document.getElementById('resultado');
-    p.innerText = 'Contegem: ';
+    p.innerText = 'Contagem: ';
 
-    if(inicio < fim){
+    if(passos == 0){
+        window.alert('O passo deve ser maior que 0!');
+    }
+    else if(inicio < fim){
         for(let i=inicio;i<=fim;i+=passos){
             p.innerText += `👉 ${i}`;
         }
