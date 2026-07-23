a878faa2fcb67b724e9b372059c19df7b6c60b8b
JadielSilva11 <jdielcunha@gmail.com>
2026-07-23 17:58:48 -0300

    Exercício da aula 6

---
+++ exercicios/aula 5/aula5.js
@@ -0,0 +1,17 @@
+const idade = 18;
+
+if(idade >= 18){
+    console.log("Permitido!");
+} else {
+    console.log("Negado");
+}
+
+const nota = 7;
+
+if(nota >= 9){
+    console.log("Excelente!");
+} else if(nota >= 7){
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
