c32b99b4f79cffc5448820e3b67b062b05e64124
JadielSilva11 <jdielcunha@gmail.com>
2026-07-23 18:00:03 -0300

    Exercício da aula 6

---
+++ exercicios/aula 7/aula7.js
@@ -0,0 +1,21 @@
+function saudacao(){
+    console.log("Olá, mundo!");
+}
+
+function mostrarNome(nome){
+    console.log(nome);
+}
+
+function soma(a, b){
+    return a + b;
+}
+
+function ehMaiorDeIdade(idade){
+    return idade >= 18;
+}
+
+saudacao();
+mostrarNome("Jadiel");
+const result = soma(5, 15);
+console.log(result);
+console.log(ehMaiorDeIdade(7)); 
