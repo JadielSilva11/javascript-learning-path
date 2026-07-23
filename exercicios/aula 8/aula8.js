7e2076ee9248acdfb931c47eef08531b5e5c3675
JadielSilva11 <jdielcunha@gmail.com>
2026-07-23 18:00:11 -0300

    Exercício da aula 8

---
+++ exercicios/aula 8/aula8.js
@@ -0,0 +1,19 @@
+const sistema = "Atendimento";
+
+function exibir(){
+    console.log(sistema);
+}
+
+exibir();
+
+function soma(){
+    const soma = 4 + 3;
+    console.log(soma);
+}
+
+soma();
+
+if(true){
+    let nome = "Jadiel";
+    console.log(nome);
+}
