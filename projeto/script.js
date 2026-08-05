b616a1734546407deef94558ea5e8b8b0d25e2e5
JadielSilva11 <jdielcunha@gmail.com>
2026-08-05 16:15:16 -0300

    Atualizando o projeto do curso ao final do Módulo 2

--- projeto/script.js
+++ projeto/script.js
@@ -1,15 +1,22 @@
 const chats = [];
 
-function createChat(id, customer){
-    newChat = {
+function createChat(id, user){
+    const newChat = {
         id: id,
-        customer: customer,
+        user: user,
         messages: [],
-        qtdMessages: 0,
-        status: 'PENDENTE'
+        status: 'OPEN'
     };
 
     chats.push(newChat);
+}
+
+function findChatById(chatId){
+    for(const chat of chats){
+        if(chat.id === chatId){
+            return chat;
+        }
+    }
 }
 
 function listChats(){
@@ -18,46 +25,108 @@ function listChats(){
     }
 }
 
+function sortChatsByMessages(){
+    chats.sort((a, b) => b.messages.length - a.messages.length);
+    console.log(chats);
+}
+
+function listOpenChats(){
+    for(const chat of chats){
+        if(chat.status === "OPEN"){
+            console.log(chat);
+        }
+    }
+}
+
+function endChat(chatId){
+    const chat = findChatById(chatId);
+    if(!chat){
+        console.log("Chat não encontrado!");
+        return;
+    }
+
+    chat.status = 'CLOSED';
+
+}
+
 function addMessage(id, chatId, sender, content, time){
-    chatExists(chatId);
-    newMessage = {
+    const chat = findChatById(chatId);
+    if(!chat){
+        console.log("Chat não encontrado!");
+        return;
+    }
+
+    const newMessage = {
         id: id,
         sender: sender,
         content: content,
         time: time
     };
 
-    for(const chat of chats){
-        if(chat.id === chatId){
-            chat.messages.push(newMessage);
-            chat.qtdMessages++;
-        }
-    }
+    chat.messages.push(newMessage);
 }
 
 function listMessages(chatId){
-    chatExists(chatId);
-    for(const chat of chats){
-        if(chat.id === chatId){
-            console.log(Object.entries(chat));
-        }
+    const chat = findChatById(chatId);
+    if(!chat){
+        console.log("Chat não encontrado!");
+        return;
+    }
+
+    for(const message of chat.messages){
+        console.log(`${message.sender}: ${message.content}`);
     }
 }
 
-function endChat(chatId){
-    chatExists(chatId);
+function totalSystemMessages(){
+    let totalMessages = 0;
     for(const chat of chats){
-        if(chat.id === chatId){
-            chat.status = 'CONCLUIDO';
-        }
+        totalMessages += chat.messages.length;
+    }
+    return totalMessages;
+}
+
+function userWithMostMessages(){
+    listChatsByMessages();
+    console.log(`Cliente com mais mensagens ${chats[0].user}`);
+}
+
+function showChatAttributes(chatId){
+    const chat = findChatById(chatId);
+    if(!chat){
+        console.log("Chat não encontrado!");
+        return;
+    }
+
+    console.log(Object.keys(chat));
+}
+
+function showChatValues(chatId){
+    const chat = findChatById(chatId);
+    if(!chat){
+        console.log("Chat não encontrado!");
+        return;
+    }
+
+    console.log(Object.values(chat));
+}
+
+function showChatDetails(chatId){
+    const chat = findChatById(chatId);
+    if(!chat){
+        console.log("Chat não encontrado!");
+        return;
+    }
+
+    for(const [key, value] of Object.entries(chat)){
+        console.log(`${key}: ${value}`);
     }
 }
 
-function chatExists(chatId){
-    for(const chat of chats){
-        if(chat.id === chatId){
-            return true;
-        }
+function checkPermissions(user){
+    if(user.type === "ADMIN"){
+        console.log("Acesso liberado!");
+    }else{
+        console.log("Acesso negado!");
     }
-    return false;
 }
